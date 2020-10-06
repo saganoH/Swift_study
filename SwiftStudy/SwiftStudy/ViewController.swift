317dc50b624ddb63aa55e94f0c5c0d950c7a9184
sagano <hikaru.sagano@jp.ricoh.com>
2020-10-06 13:21:31 +0900

    レンジ演算子サンプルコード

--- SwiftStudy/SwiftStudy/ViewController.swift
+++ SwiftStudy/SwiftStudy/ViewController.swift
@@ -12,12 +12,23 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("ありがとう")
+        
+        //p68List
+        //レンジ演算子
+        
+        //整数の範囲
+        let rangeInt = -5..<5
+        print(rangeInt.contains(-3))
+        print(rangeInt.contains(2))
+        print(rangeInt.contains(5)) //範囲外
+        print("--------------------------")
+        //実数の範囲
+        let rangeDouble = 0.0...1.0
+        print(rangeDouble.contains(0.1))
+        print(rangeDouble.contains(1.0))
+        print(rangeDouble.contains(1.5)) //範囲外
 
-        print("こんにちは")
-
-
-        // for conflict
+       
 
     }
 
