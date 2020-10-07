4a20eef9793a4f7080b51e348bd9b67f49e3b44f
sagano <hikaru.sagano@jp.ricoh.com>
2020-10-07 14:22:37 +0900

    sec2-5

--- SwiftStudy/SwiftStudy/ViewController.swift
+++ SwiftStudy/SwiftStudy/ViewController.swift
@@ -4,7 +4,6 @@
 //
 //  Created by sagano hikaru on 2020/10/05.
 //
-
 import UIKit
 
 class ViewController: UIViewController {
@@ -12,47 +11,103 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        checkRangeInt(value: -3)
-        checkRangeInt(value: 2)
-        checkRangeInt(value: 5)
-        
-        checkRangeDouble(0.1)
-        checkRangeDouble(1.0)
-        checkRangeDouble(1.5)
-        
-        changeColor()
+        ifStatement()
+        ifElseStatement()
+        ifElseIfStatement()
+        ifNesting()
+        ifAnd()
+        ifOr()
+        ifComma()
         
     }
     
-    //p68List　レンジ演算子の変化系
-    func checkRangeInt(value : Int){
-        let rangeInt = -5..<5
-        print(rangeInt.contains(value))
+    //p73List　if文
+    func ifStatement(){
+        let tokuten = 85
+        if tokuten>=80 {
+            print("素晴らしい!")
+        }
+        print("\(tokuten)点でした。")
     }
-
-    func checkRangeDouble(_ value : Double){
-        let rangeDouble = 0.0...1.0
-        print(rangeDouble.contains(value))
-    }
-
     
-    func changeColor(){
-        //p70List ビット演算子
-        //16進数RRGGBBをR、G、Bに分解
-        let RGB: UInt32 = 0x40E0D0
-        let red = (RGB & 0xFF0000) >> 16
-        let green = (RGB & 0x00FF00) >> 8
-        let blue = RGB & 0x0000FF
-        print("red \(red), green \(green), blue \(blue)")
-            
-        //背景色に設定
-        let R = CGFloat(red)/255
-        let G = CGFloat(green)/255
-        let B = CGFloat(blue)/255
-        view.backgroundColor = UIColor(red:R, green:G, blue:B, alpha:1)
-
+    //p74List if-else文
+    func ifElseStatement(){
+        let tokuten = arc4random_uniform(101)
+        if tokuten>=60 {
+            print("おめでとう！合格です。")
+        } else {
+            print("残念、不合格です。")
+        }
+        print("\(tokuten)点でした。")
     }
 
+    //p75List if-else-if文
+    func ifElseIfStatement(){
+        let tokuten = arc4random_uniform(101)
+        if tokuten<30 {
+            print("がんばりましょう")
+        } else if tokuten<80{
+            print("合格です")
+        } else {
+            print("素晴らしい！")
+        }
+        print("\(tokuten)点でした。")
+    }
+    
+    //p75List ネスティング
+    func ifNesting(){
+        let sugaku = arc4random_uniform(101)
+        let eigo = arc4random_uniform(101)
+        
+        if sugaku>=50 {
+            if eigo>=60 {
+                print("おめでとう！合格")
+            } else {
+                print("残念、不合格")
+            }
+        } else {
+            print("残念、不合格")
+        }
+        print("数学\(sugaku)、英語\(eigo)")
+    }
+    
+    //p76List if and
+    func ifAnd(){
+        let sugaku = arc4random_uniform(101)
+        let eigo = arc4random_uniform(101)
+        
+        if (sugaku>=50) && (eigo>=60) {
+            print("おめでとう！合格")
+        } else {
+            print("残念、不合格")
+        }
+        print("数学\(sugaku)、英語\(eigo)")
+    }
+    
+    //p75List if or
+    func ifOr(){
+        let sugaku = arc4random_uniform(101)
+        let eigo = arc4random_uniform(101)
+        
+        if (sugaku>=60) || (eigo>=60) {
+            print("おめでとう！合格")
+        } else {
+            print("残念、不合格")
+        }
+        print("数学\(sugaku)、英語\(eigo)")
+    }
+    
+    //p76List ifカンマ区切り
+    func ifComma(){
+        let sugaku = 56
+        let eigo = 62
+        
+        if sugaku>=50, eigo>=50, (sugaku+eigo)>=120 {
+            print("合格", terminator: "／") //到達不能
+        } else {
+            print("不合格", terminator: "／")
+        }
+        print("数学\(sugaku)、英語\(eigo)、合計\(sugaku+eigo)")
+    }
 
 }
-
