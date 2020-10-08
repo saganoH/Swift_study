039f3bf284ebda19cf751ff360e097c857fffb64
sagano <hikaru.sagano@jp.ricoh.com>
2020-10-08 16:10:20 +0900

    codeClean

---
+++ SwiftStudy/SwiftStudy/OperatorSample.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+class OperatorSample: UIViewController {
+    //p68List　レンジ演算子の変化系
+    func checkRangeInt(value: Int) {
+        let rangeInt = -5..<5
+        print(rangeInt.contains(value))
+    }
+    
+    func checkRangeDouble(_ value: Double) {
+        let rangeDouble = 0.0...1.0
+        print(rangeDouble.contains(value))
+    }
+
+    //p70List ビット演算子
+    func changeColor() {
+        //16進数RRGGBBをR、G、Bに分解
+        let RGB: UInt32 = 0x40E0D0
+        let red = (RGB & 0xFF0000) >> 16
+        let green = (RGB & 0x00FF00) >> 8
+        let blue = RGB & 0x0000FF
+        print("red \(red), green \(green), blue \(blue)")
+        
+        //背景色に設定
+        let R = CGFloat(red)/255
+        let G = CGFloat(green)/255
+        let B = CGFloat(blue)/255
+        view.backgroundColor = UIColor(red: R, green: G, blue: B, alpha: 1)
+    }
+}
