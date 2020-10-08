4c8181dfda6c4e09a74744718cde01a2dc84131f
sagano <hikaru.sagano@jp.ricoh.com>
2020-10-08 10:17:29 +0900

    コード規約関連修正

--- SwiftStudy/SwiftStudy/ViewController.swift
+++ SwiftStudy/SwiftStudy/ViewController.swift
@@ -1,15 +1,10 @@
-//
-//  ViewController.swift
-//  SwiftStudy
-//
-//  Created by sagano hikaru on 2020/10/05.
-//
 import UIKit
 
 class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         checkRangeInt(value: -3)
         checkRangeInt(value: 2)
         checkRangeInt(value: 5)
@@ -29,61 +24,62 @@ class ViewController: UIViewController {
         ifNesting(score.sugaku, score.eigo)
         ifAnd(score.sugaku, score.eigo)
         ifOr(score.sugaku, score.eigo)
-        ifComma()
+        ifComma(sugaku: score.sugaku, eigo: score.eigo)
         
     }
     
     //p68List　レンジ演算子の変化系
-       func checkRangeInt(value : Int){
-           let rangeInt = -5..<5
-           print(rangeInt.contains(value))
-       }
-    func checkRangeDouble(_ value : Double){
-            let rangeDouble = 0.0...1.0
-            print(rangeDouble.contains(value))
-    }
-    func changeColor(){
-            //p70List ビット演算子
-            //16進数RRGGBBをR、G、Bに分解
-            let RGB: UInt32 = 0x40E0D0
-            let red = (RGB & 0xFF0000) >> 16
-            let green = (RGB & 0x00FF00) >> 8
-            let blue = RGB & 0x0000FF
-            print("red \(red), green \(green), blue \(blue)")
-
-            //背景色に設定
-            let R = CGFloat(red)/255
-            let G = CGFloat(green)/255
-            let B = CGFloat(blue)/255
-            view.backgroundColor = UIColor(red:R, green:G, blue:B, alpha:1)
+    func checkRangeInt(value: Int){
+        let rangeInt = -5..<5
+        print(rangeInt.contains(value))
     }
     
+    func checkRangeDouble(_ value: Double){
+        let rangeDouble = 0.0...1.0
+        print(rangeDouble.contains(value))
+    }
+
+    //p70List ビット演算子
+    func changeColor(){
+        //16進数RRGGBBをR、G、Bに分解
+        let RGB: UInt32 = 0x40E0D0
+        let red = (RGB & 0xFF0000) >> 16
+        let green = (RGB & 0x00FF00) >> 8
+        let blue = RGB & 0x0000FF
+        print("red \(red), green \(green), blue \(blue)")
+        
+        //背景色に設定
+        let R = CGFloat(red)/255
+        let G = CGFloat(green)/255
+        let B = CGFloat(blue)/255
+        view.backgroundColor = UIColor(red: R, green: G, blue: B, alpha: 1)
+    }
     
     //p73List　if文
-    func ifStatement(_ tokuten:UInt32){
+    func ifStatement(_ tokuten: UInt32){
         let tokuten = 85
-        if tokuten>=80 {
+        if tokuten >= 80 {
             print("素晴らしい!")
         }
         print("\(tokuten)点でした。")
     }
     
     //p74List if-else文
-    func ifElseStatement(_ tokuten:UInt32){
+    func ifElseStatement(_ tokuten: UInt32){
         
-        if tokuten>=60 {
+        if tokuten >= 60 {
             print("おめでとう！合格です。")
         } else {
             print("残念、不合格です。")
         }
         print("\(tokuten)点でした。")
     }
-
+    
     //p75List if-else-if文
-    func ifElseIfStatement(_ tokuten:UInt32){
-        if tokuten<30 {
+    func ifElseIfStatement(_ tokuten: UInt32){
+        if tokuten < 30 {
             print("がんばりましょう")
-        } else if tokuten<80{
+        } else if tokuten < 80 {
             print("合格です")
         } else {
             print("素晴らしい！")
@@ -92,9 +88,9 @@ class ViewController: UIViewController {
     }
     
     //p75List ネスティング
-    func ifNesting(_ sugaku:UInt32,_ eigo:UInt32){
-        if sugaku>=50 {
-            if eigo>=60 {
+    func ifNesting(_ sugaku: UInt32,_ eigo: UInt32){
+        if sugaku >= 50 {
+            if eigo >= 60 {
                 print("おめでとう！合格")
             } else {
                 print("残念、不合格")
@@ -106,8 +102,8 @@ class ViewController: UIViewController {
     }
     
     //p76List if and
-    func ifAnd(_ sugaku:UInt32,_ eigo:UInt32){
-        if (sugaku>=50) && (eigo>=60) {
+    func ifAnd(_ sugaku: UInt32,_ eigo: UInt32){
+        if (sugaku >= 50) && (eigo >= 60) {
             print("おめでとう！合格")
         } else {
             print("残念、不合格")
@@ -116,8 +112,8 @@ class ViewController: UIViewController {
     }
     
     //p75List if or
-    func ifOr(_ sugaku:UInt32,_ eigo:UInt32){
-        if (sugaku>=60) || (eigo>=60) {
+    func ifOr(_ sugaku: UInt32,_ eigo: UInt32){
+        if (sugaku >= 60) || (eigo >= 60) {
             print("おめでとう！合格")
         } else {
             print("残念、不合格")
@@ -126,12 +122,9 @@ class ViewController: UIViewController {
     }
     
     //p76List ifカンマ区切り
-    func ifComma(){
-        let sugaku = 56
-        let eigo = 62
-        
-        if sugaku>=50, eigo>=50, (sugaku+eigo)>=120 {
-            print("合格", terminator: "／") //到達不能
+    func ifComma(sugaku: UInt32, eigo: UInt32){
+        if sugaku >= 50, eigo >= 50, (sugaku+eigo) >= 120 {
+            print("合格", terminator: "／")
         } else {
             print("不合格", terminator: "／")
         }
