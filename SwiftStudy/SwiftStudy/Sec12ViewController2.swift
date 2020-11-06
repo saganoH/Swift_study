a4ea7423d79ae58f4c88cd3a947b9dfa2e3a9a40
sagano <hikaru.sagano@jp.ricoh.com>
2020-11-06 16:12:00 +0900

    [fix] No.73 Chapter12-1
    * コンフリクトの解消

--- SwiftStudy/SwiftStudy/Sec12ViewController2.swift
+++ SwiftStudy/SwiftStudy/Sec12ViewController2.swift
@@ -7,6 +7,11 @@ class Sec12ViewController2: UIViewController {
     @IBOutlet weak var okButton: UIButton!
     
     @IBAction func random(_ sender: Any) {
+        //乱数を作る
+        let num = arc4random_uniform(100)
+        numLabel.text = String(num)
+        //ボタンの有効・無効の設定
+        okButton.isEnabled = (num >= 50)
     }
     
     @IBAction func tapOK(_ sender: Any) {
@@ -20,12 +25,7 @@ class Sec12ViewController2: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        //乱数を作る
-        let num = arc4random_uniform(100)
-        numLabel.text = String(num)
         okButton.frame = CGRect(x: 100, y: 200, width: 50, height: 50)
-        //ボタンの有効・無効の設定
-        okButton.isEnabled = (num >= 50)
         
         //ノーマル・ハイライトの画像の設定
         let myButton = UIButton(type: .custom)
