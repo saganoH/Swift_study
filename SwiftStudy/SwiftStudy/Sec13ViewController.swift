8c9c0a20e83b89a1efcc261bb6f8f026cc3c697f
sagano <hikaru.sagano@jp.ricoh.com>
2020-11-12 16:43:54 +0900

    [story] No.74 Chapter13
    * ビューの作成と表示
    * サブビューの入れ子構造

--- SwiftStudy/SwiftStudy/Sec13ViewController.swift
+++ SwiftStudy/SwiftStudy/Sec13ViewController.swift
@@ -1,4 +1,27 @@
-class Sec13ViewController {
-    
-    
+import UIKit
+
+class Sec13ViewController: UIViewController {
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        //ビューを作る
+        let theView = UIView()
+        //ビューの領域と背景色
+        theView.frame = CGRect(x: 60, y: 100, width: 210, height: 200)
+        theView.backgroundColor = UIColor(red: 0.9, green: 0.9, blue: 0.6, alpha: 1.0)
+        
+        //ラベルを作る
+        let labelA = UILabel()
+        labelA.text = "ラベルA"
+        //ラベルの領域
+        labelA.frame = CGRect(x: 50, y: 60, width: 110, height: 21)
+        //背景色と文字色
+        labelA.backgroundColor = UIColor.orange
+        labelA.textColor = UIColor.white
+        
+        //ラベルをビューに追加する
+        theView.addSubview(labelA)
+        self.view.addSubview(theView)
+    }
 }
