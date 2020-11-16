eaef079f3e36695d6c80cce60dab2bf1a3b67666
sagano <hikaru.sagano@jp.ricoh.com>
2020-11-16 13:55:59 +0900

    [story] No.74 Chapter13
    * スタックビューの作成

---
+++ SwiftStudy/SwiftStudy/SecViewController4.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class Sec13ViewController4: UIViewController {
+
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+       
+    }
+}
