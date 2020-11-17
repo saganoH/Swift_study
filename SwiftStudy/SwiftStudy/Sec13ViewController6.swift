5c0680aed29e0d83ca995f6ecc8074fd67b410b9
sagano <hikaru.sagano@jp.ricoh.com>
2020-11-17 11:16:55 +0900

    [story] No.74 Chapter13(2)
    * コントローラ画面の追加

---
+++ SwiftStudy/SwiftStudy/Sec13ViewController6.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class Sec13ViewController6: UIViewController {
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+       
+    }
+}
