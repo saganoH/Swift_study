2856450c05311f9eda163fd3e89b3a93f10dd158
sagano <hikaru.sagano@jp.ricoh.com>
2020-11-17 14:12:24 +0900

    [fix] No.74 Chapter13修正
    * privateの指定
    * tableDataをクラスプロパティに変更

--- SwiftStudy/SwiftStudy/Sec13ViewController5.swift
+++ SwiftStudy/SwiftStudy/Sec13ViewController5.swift
@@ -1,13 +1,13 @@
 import UIKit
 
-private let sectionTitle = ["チョウ目", "バッタ目", "コウチュウ目"]
-private let section0 = [("キタテハ", "タテハチョウ科"), ("クロアゲハ", "アゲハチョウ科")]
-private let section1 = [("キリギリス", "キリギリス科"), ("ヒナバッタ", "バッタ科"), ("マツムシ", "マツムシ科")]
-private let section2 = [("ハンミョウ", "ハンミョウ科"), ("アオオサムシ", "オサムシ科"), ("チビクワガタ", "クワガタムシ科")]
-private var tableData = [section0, section1, section2]
-
 class Sec13ViewController5: UIViewController, UITableViewDelegate, UITableViewDataSource {
   
+    private static let sectionTitle = ["チョウ目", "バッタ目", "コウチュウ目"]
+    private static let section0 = [("キタテハ", "タテハチョウ科"), ("クロアゲハ", "アゲハチョウ科")]
+    private static let section1 = [("キリギリス", "キリギリス科"), ("ヒナバッタ", "バッタ科"), ("マツムシ", "マツムシ科")]
+    private static let section2 = [("ハンミョウ", "ハンミョウ科"), ("アオオサムシ", "オサムシ科"), ("チビクワガタ", "クワガタムシ科")]
+    private static var tableData = [section0, section1, section2]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -28,24 +28,24 @@ class Sec13ViewController5: UIViewController, UITableViewDelegate, UITableViewDa
     // UITableViewDataSourseプロトコル
     // セクションの個数を決める
     func numberOfSections(in tableView: UITableView) -> Int {
-        return sectionTitle.count
+        return Sec13ViewController5.sectionTitle.count
     }
     
     // セクションごとの行数を決める
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        let sectionData = tableData[section]
+        let sectionData = Sec13ViewController5.tableData[section]
         return sectionData.count
     }
     
     // セクションのタイトルを決める
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return sectionTitle[section]
+        return Sec13ViewController5.sectionTitle[section]
     }
     
     // セルを作る
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
-        let sectionData = tableData[(indexPath as NSIndexPath).section]
+        let sectionData = Sec13ViewController5.tableData[(indexPath as NSIndexPath).section]
         let cellData = sectionData[(indexPath as NSIndexPath).row]
         cell.textLabel?.text = cellData.0
         cell.detailTextLabel?.text = cellData.1
@@ -55,8 +55,8 @@ class Sec13ViewController5: UIViewController, UITableViewDelegate, UITableViewDa
     // UITableViewDelegateメソッド
     // 行がタップされると実行される
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let title = sectionTitle[indexPath.section]
-        let sectionData = tableData[indexPath.section]
+        let title = Sec13ViewController5.sectionTitle[indexPath.section]
+        let sectionData = Sec13ViewController5.tableData[indexPath.section]
         let cellData = sectionData[indexPath.row]
         print("\(title) \(cellData.1)")
         print("\(cellData.0)")
