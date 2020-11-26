9cede8ca7cc0a76f0785e8964af06ad32e973a14
sagano <hikaru.sagano@jp.ricoh.com>
2020-11-26 16:00:20 +0900

    [story] No.75 Chapter14
    * アラートの実装

--- SwiftStudy/SwiftStudy/Sec14ViewController.swift
+++ SwiftStudy/SwiftStudy/Sec14ViewController.swift
@@ -6,4 +6,61 @@ class Sec14ViewController: UIViewController {
         // 現在のシーンを閉じて元のシーンに戻る
         self.dismiss(animated: true, completion: nil)
     }
+    
+    @IBAction func showAlert(_ sender: Any) {
+        // アラートを作る
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .alert)
+        alert.title = "タイトル"
+        alert.message = "メッセージ"
+        
+        // ボタン１
+        alert.addAction(
+            UIAlertAction(
+                title: "ボタン１",
+                style: .default,
+                handler: {(action) -> Void in self.hello(action.title!)}
+            )
+        )
+        
+        // ボタン２
+        alert.addAction(
+            UIAlertAction(
+                title: "ボタン２",
+                style: .default,
+                handler: {(action) -> Void in self.hello(action.title!)}
+            )
+        )
+        
+        // キャンセル
+        alert.addAction(
+            UIAlertAction(
+                title: "キャンセル",
+                style: .cancel,
+                handler: nil)
+        )
+        
+        // 赤色のボタン
+        alert.addAction(
+            UIAlertAction(
+                title: "削除します",
+                style: .destructive,
+                handler: {(action) -> Void in self.hello(action.title!)}
+            )
+        )
+        
+        // アラートを表示する
+        self.present(
+            alert,
+            animated: true,
+            completion: {
+                // 表示完了後に実行
+                print("アラートが表示された")
+            }
+        )
+    }
+    
+    func hello(_ message: String) {
+        print(message)
+    }
+    
 }
