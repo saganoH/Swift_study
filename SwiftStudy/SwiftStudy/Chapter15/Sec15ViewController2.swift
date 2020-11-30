7fb7db23ccd7a63126c4d1fee2a503664938da56
sagano <hikaru.sagano@jp.ricoh.com>
2020-11-30 13:30:05 +0900

    [story] No.90 Chapter15
    * イーズインアウトのアニメーション

---
+++ SwiftStudy/SwiftStudy/Chapter15/Sec15ViewController2.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+class Sec15ViewController2: UIViewController {
+    
+    @IBOutlet weak var azarashi: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // ビューがタップされたら実行
+    @IBAction func tapView(_ sender: UITapGestureRecognizer) {
+        //タップされた座標を調べる
+        let tapPoint = sender.location(in: view)
+        
+        // 指定した最終値になるようにアニメーションする
+        UIView.animate(
+            withDuration: 1.0,
+            delay: 0,
+            options: [.curveEaseInOut, .autoreverse, .repeat],
+            animations: {
+                self.azarashi.center = tapPoint
+            },
+            completion: nil)
+    }
+}
