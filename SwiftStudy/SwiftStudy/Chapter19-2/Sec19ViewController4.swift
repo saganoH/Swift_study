b5afe7c391ee52569157d61060e382fbfb4f5213
sagano <hikaru.sagano@jp.ricoh.com>
2020-12-16 16:44:29 +0900

    [story] No.109 No.Chapter19-2
    * locationManager
    * 位置情報のプライバシー設定
    (途中）

--- SwiftStudy/SwiftStudy/Chapter19-2/Sec19ViewController4.swift
+++ SwiftStudy/SwiftStudy/Chapter19-2/Sec19ViewController4.swift
@@ -1,7 +1,7 @@
 import UIKit
+import CoreLocation
 
-// 19-6 位置情報とコンパス
-class Sec19ViewController4: UIViewController {
+class Sec19ViewController4: UIViewController, CLLocationManagerDelegate {
     
     override var shouldAutorotate: Bool {
         return true
@@ -11,5 +11,85 @@ class Sec19ViewController4: UIViewController {
         return .portrait
     }
     
+    @IBOutlet weak var idoLabel: UILabel!
+    @IBOutlet weak var keidoLabel: UILabel!
+    @IBOutlet weak var hyoukouLabel: UILabel!
+    @IBOutlet weak var houiLabel: UILabel!
+    @IBOutlet weak var jihokuSeg: UISegmentedControl!
+    @IBOutlet weak var compass: UIImageView!
     
+    // ロケーションマネージャを作る
+    var locationManager = CLLocationManager()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        disabledLocationLabel()
+        // 位置情報の利用許可を得る
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.delegate = self
+        setupLocationService()
+        startHeadingService()
+    }
+    
+    // ロケーション機能の設定
+    func setupLocationService() {
+        // ロケーションの精度を設定する
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        // 更新距離(m)
+        locationManager.distanceFilter = 1
+    }
+    
+    func disabledLocationLabel() {
+        let message = "位置情報の利用が許可されていない"
+        idoLabel.text = message
+        keidoLabel.text = message
+        hyoukouLabel.text = message
+    }
+    
+    func startHeadingService() {
+        
+    }
+    
+    // MARK: - CLLocationManagerDelegate
+    
+    // 位置情報利用許可のステータスが変わった場合
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .authorizedAlways, .authorizedWhenInUse:
+            // ロケーションの更新を開始する
+            locationManager.startUpdatingLocation()
+        case .notDetermined:
+            // ロケーションの更新を停止する
+            locationManager.stopUpdatingLocation()
+            disabledLocationLabel()
+        default:
+            // ロケーションの更新を停止する
+            locationManager.stopUpdatingLocation()
+            disabledLocationLabel()
+        }
+    }
+    
+    // 位置情報が更新された場合
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let locationData = locations.last
+        
+        if var ido = locationData?.coordinate.latitude {
+            ido = round( (ido*1000000) / 1000000 )
+            idoLabel.text = String(ido)
+        }
+        
+        if var keido = locationData?.coordinate.longitude {
+            keido = round( (keido*1000000) / 1000000 )
+            keidoLabel.text = String(keido)
+        }
+        
+        if var hyoukou = locationData?.altitude {
+            hyoukou = round( (hyoukou*100) / 100 )
+            hyoukouLabel.text = String(hyoukou) + "m"
+        }
+    }
+    
+    // 向いている方角が変わった
+    func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
+        
+    }
 }
