1b5c1a4ea97e386de253bc0d30d3eec3fff3ec06
sagano <hikaru.sagano@jp.ricoh.com>
2020-12-22 11:39:09 +0900

    [story] No.109 Chapter19-2
    * 長押しでピンの追加
    * ピンのカスタマイズ

--- SwiftStudy/SwiftStudy/Chapter19-2/Sec19ViewController7.swift
+++ SwiftStudy/SwiftStudy/Chapter19-2/Sec19ViewController7.swift
@@ -1,12 +1,16 @@
 import UIKit
 import MapKit
 
-class Sec19ViewController7: UIViewController {
+class Sec19ViewController7: UIViewController, MKMapViewDelegate {
     
     @IBOutlet weak var myMap: MKMapView!
+    private var annotationlist = Array<MKPointAnnotation>()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        myMap.delegate = self
+        
         // 横浜中華街の座標を作る
         let chinaTownCenter = CLLocationCoordinate2D(latitude: 35.4424225, longitude: 139.6465645)
         let span = MKCoordinateSpan(latitudeDelta: 0.002, longitudeDelta: 0.002)
@@ -19,5 +23,39 @@ class Sec19ViewController7: UIViewController {
         annotation.title = "中華街"
         annotation.subtitle = "待ち合わせ場所"
         myMap.addAnnotation(annotation)
+    }
+    
+    // MARK: - @IBAction
+    
+    @IBAction func goBack(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func mapLongPress(_ sender: UILongPressGestureRecognizer) {
+        // 長押しの終了でのみ実行する
+        guard sender.state == UIGestureRecognizer.State.ended else {
+            return
+        }
+        // 長押しした座標を取り出す
+        let pressPoint = sender.location(in: myMap)
+        // マップの座標に変換
+        let pressCoodinate = myMap.convert(pressPoint, toCoordinateFrom: myMap)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = pressCoodinate
+        annotation.title = "ここ！"
+        annotationlist.append(annotation)
+        myMap.addAnnotation(annotation)
+    }
+    
+    // MARK: - MKMapViewDelegate
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        // ピンをカスタマイズする
+        let pinView = MKPinAnnotationView()
+        pinView.animatesDrop = true
+        pinView.isDraggable = true
+        pinView.pinTintColor = UIColor.blue
+        pinView.canShowCallout = true
+        return pinView
     }
 }
