4a410795c03636867bba5c3eb61f07ac0874a0fc
sagano <hikaru.sagano@jp.ricoh.com>
2020-12-22 14:03:06 +0900

    [story] No.109 Chapter19-2
    * 地図に直線と円を描画

--- SwiftStudy/SwiftStudy/Chapter19-2/Sec19ViewController5.swift
+++ SwiftStudy/SwiftStudy/Chapter19-2/Sec19ViewController5.swift
@@ -1,17 +1,26 @@
 import UIKit
 import MapKit
 
-class Sec19ViewController5: UIViewController {
+class Sec19ViewController5: UIViewController, MKMapViewDelegate {
     
     @IBOutlet weak var myMap: MKMapView!
     @IBOutlet weak var toolBar: UIToolbar!
     private var toolbarDefaultColor: UIColor!
+    private let tokyoPoint = CLLocationCoordinate2D(latitude: 35.690553, longitude: 139.699579)
+    private let sapporoPoint = CLLocationCoordinate2D(latitude: 43.068564, longitude: 141.3507138)
     
     override func viewWillAppear(_ animated: Bool) {
         toolbarDefaultColor = toolBar.tintColor
         // スケールを表示
         myMap.showsScale = true
         myMap.mapType = .standard
+        myMap.delegate = self
+        let locations = [tokyoPoint, sapporoPoint]
+        for location in locations {
+            let circle = MKCircle(center: location, radius: 30000)
+            myMap.addOverlay(circle)
+        }
+        myMap.addOverlay(MKPolyline(coordinates: locations, count: locations.count))
     }
   
     // MARK: - @IBAction
@@ -28,9 +37,11 @@ class Sec19ViewController5: UIViewController {
         let mapRegion = MKCoordinateRegion(center: mapCenter, span: mapSpan)
         // 領域の地図を表示する
         myMap.setRegion(mapRegion, animated: true)
+        myMap.removeOverlays(myMap.overlays)
     }
     
     @IBAction func changeMapType(_ sender: UISegmentedControl) {
+        myMap.removeOverlays(myMap.overlays)
         switch sender.selectedSegmentIndex {
         case 0:
             myMap.mapType = .standard
@@ -55,5 +66,26 @@ class Sec19ViewController5: UIViewController {
         default:
             break
         }
+    }
+    
+    // MARK: - MKMapViewDelegate
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let renderer: MKOverlayPathRenderer
+        switch overlay {
+        case is MKPolyline:
+            renderer = MKPolylineRenderer(overlay: overlay)
+            renderer.lineWidth = 2
+            renderer.strokeColor = UIColor.blue
+        case is MKCircle:
+            renderer = MKCircleRenderer(circle: overlay as! MKCircle)
+            renderer.lineWidth = 2
+            renderer.strokeColor = UIColor.red
+            renderer.fillColor = UIColor.red
+            renderer.alpha = 0.3
+        default:
+            renderer = MKPolylineRenderer(overlay: overlay)
+        }
+        return renderer
     }
 }
