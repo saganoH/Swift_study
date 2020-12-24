b11ffbdd1aa1fa96f431f1ee2ee440d6050ed4d3
sagano <hikaru.sagano@jp.ricoh.com>
2020-12-24 17:01:05 +0900

    Chapter17の設計修正

--- SwiftStudy/SwiftStudy/Chapter19-2/Chapter17/Sec17ViewController2.swift
+++ SwiftStudy/SwiftStudy/Chapter19-2/Chapter17/Sec17ViewController2.swift
@@ -4,52 +4,10 @@ class Sec17ViewController2: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let drawC = drawPresenter(viewCenter: view.center)
         
-        let percentDouble = makeRandomNum()
-        let drawImage = drawLine(percent: percentDouble)
+        let drawImage = drawC.drawLine(size: view.bounds.size)
         let drawView = UIImageView(image: drawImage)
         view.addSubview(drawView)
-    }
-    
-    private func arcPercent(_ radius: CGFloat, _ percent: Double) -> UIBezierPath {
-        let endAngle = (2 * Double.pi * percent/100) - (Double.pi/2)
-        let path = UIBezierPath(
-            arcCenter: view.center,
-            radius: radius,
-            startAngle: CGFloat(-Double.pi/2),
-            endAngle: CGFloat(endAngle),
-            clockwise: percent > 0)
-        return path
-    }
-    
-    private func drawLine(percent: Double) -> UIImage {
-        let size = view.bounds.size
-        UIGraphicsBeginImageContextWithOptions(size, false, 1.0)
-        
-        // 円弧のパスを作る
-        UIColor.red.setStroke()
-        let arcPath = arcPercent(80, percent)
-        arcPath.lineWidth = 60
-        arcPath.lineCapStyle = .butt
-        arcPath.stroke()
-        
-        // %のラベル
-        let font = UIFont.boldSystemFont(ofSize: 28)
-        let textFontAttributes = [NSAttributedString.Key.font: font,
-                                  NSAttributedString.Key.foregroundColor: UIColor.gray]
-        let drawString = String(percent) + "%"
-        let posX = view.center.x - 45
-        let posY = view.center.y - 15
-        let rect = CGRect(x: posX, y: posY, width: 90, height: 30)
-        drawString.draw(in: rect, withAttributes: textFontAttributes)
-        
-        let image = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        return image!
-    }
-    
-    private func makeRandomNum() -> Double {
-        let randomDouble = Double.random(in: 0.0...100.0)
-        return randomDouble
     }
 }
