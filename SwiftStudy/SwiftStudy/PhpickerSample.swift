c7c0e4f760fd719222782451d2748036d595d4e4
sagano <hikaru.sagano@jp.ricoh.com>
2021-01-05 15:23:43 +0900

    PHPicker使い方テスト

---
+++ SwiftStudy/SwiftStudy/PhpickerSample.swift
@@ -0,0 +1,49 @@
+import UIKit
+import PhotosUI
+
+// PHPickerの使い方テスト
+class PhpickerSample: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Pickerの設定
+        var config = PHPickerConfiguration(photoLibrary: PHPhotoLibrary.shared())
+
+        // Pickerで選択するメディアの種類をフィルタリング
+        config.filter = .images // videos, livePhotos
+
+        // 選択できる数を指定
+        config.selectionLimit = 1
+
+        // configを渡して初期化
+        let controller = PHPickerViewController(configuration: config)
+
+        // delegateをセット
+        controller.delegate = self
+
+        // 表示
+        present(controller, animated: true)
+    }
+}
+
+extension PhpickerSample: PHPickerViewControllerDelegate {
+    // Pickerを閉じたときに呼ばれる
+    public func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        
+        picker.dismiss(animated: true)
+        
+        // PHPickerResultからImageを読み込む
+        let itemProvider = results.first?.itemProvider
+        if let itemProvider = itemProvider, itemProvider.canLoadObject(ofClass: UIImage.self) {
+            itemProvider.loadObject(ofClass: UIImage.self) { image, error in
+                if let image = image as? UIImage {
+                    // 選択されたimageを表示
+                    self.imageView.image = image
+                }
+            }
+        }
+    }
+}
